9142612796ececa1578436ddad22f13c7971e900
alexanderOpenko <openkooleksandr@gmail.com>
2022-11-16 00:55:00 +0200

    Add icons to menu items

--- wp-content/themes/workportfolio/assets/helpers/icons.php
+++ wp-content/themes/workportfolio/assets/helpers/icons.php
@@ -1,5 +1,7 @@
 <?php function icons($icon) {
     switch($icon) {
+        case 'arrow':
+            return '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="" height="" viewBox="0 0 306 306" style="enable-background:new 0 0 306 306;" xml:space="preserve"> <g> <g id="keyboard-arrow-right"> <polygon points="58.65,267.75 175.95,153 58.65,35.7 94.35,0 247.35,153 94.35,306   "/> </g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> </svg>';
         case 'burger': 
             return '<svg xmlns="http://www.w3.org/2000/svg" fill="#00000" viewBox="0 0 50 50" width="50px"><path d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z"></path></svg>';
         break; 
@@ -8,6 +10,12 @@
         case 'submenu_arrow':
             return '<svg class="svg-icon" aria-hidden="true" role="img" focusable="false" xmlns="http://www.w3.org/2000/svg" width="20" height="12" viewBox="0 0 20 12"><polygon fill="" fill-rule="evenodd" points="1319.899 365.778 1327.678 358 1329.799 360.121 1319.899 370.021 1310 360.121 1312.121 358" transform="translate(-1310 -358)"></polygon></svg>';
         break;
-        }
+        case 'email':
+            return '<svg class="svg-inline--fa fa-envelope fa-w-16" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="envelope" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z"></path></svg>';
+        break;
+        case 'phone':
+            return '<svg class="svg-inline--fa fa-phone-alt fa-w-16" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="phone-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M497.39 361.8l-112-48a24 24 0 0 0-28 6.9l-49.6 60.6A370.66 370.66 0 0 1 130.6 204.11l60.6-49.6a23.94 23.94 0 0 0 6.9-28l-48-112A24.16 24.16 0 0 0 122.6.61l-104 24A24 24 0 0 0 0 48c0 256.5 207.9 464 464 464a24 24 0 0 0 23.4-18.6l24-104a24.29 24.29 0 0 0-14.01-27.6z"></path></svg>';
+        break;
+    }
 }
 ?>
--- wp-content/themes/workportfolio/inc/template-tags.php
+++ wp-content/themes/workportfolio/inc/template-tags.php
@@ -1,17 +1,31 @@
 <?php
 function workpportfolio_add_sub_toggles_to_main_menu($args, $item, $depth) {
-    $args->before = '<div class="itemWrapper body3">';
-    $args->after = '';
+    if ($args->menu == 18 ) {
 
-    // Add a toggle to items with children.
     if (in_array('menu-item-has-children', $item->classes, true)) {
-        // Add the sub menu toggle.
-
+        $args->before = '<div class="itemWrapper body3">';
         $args->after .= '<button data-toggle-target data-type="accordion" class="submenu_arrow">' . icons('submenu_arrow') . '</button></div>';
+        return $args;
     }
-
-    return $args;
+}
 }
 
 add_filter('nav_menu_item_args', 'workpportfolio_add_sub_toggles_to_main_menu', 10, 3);
-?>
+
+function my_nav_menu_attribs( $atts, $item, $args ) {
+    $tel_pos = strpos($atts['href'], 'tel:');
+    $contact_names = array('contacts', 'контакти', 'Контакти');
+
+    if (in_array($item->title, $contact_names)) {
+        $atts['data-toggle-target'] = 'target';
+        $atts['data-type'] = "accordion";
+    }
+
+    if ($tel_pos !== false) {
+        $atts['class'] = 'telIcon';
+    }
+
+    return $atts;
+}
+
+add_filter( 'nav_menu_link_attributes', 'my_nav_menu_attribs', 10, 3 );

160962c848e35727b6e6f2a32c5c99d319351225
alexanderOpenko <openkooleksandr@gmail.com>
2022-11-16 01:07:40 +0200

    Create slider of post images and update post page styles

--- wp-content/themes/workportfolio/assets/js/index.js
+++ wp-content/themes/workportfolio/assets/js/index.js
@@ -1,8 +1,9 @@
 workportfolio = {}
 
 function init() {
-    workportfolio.toggles.init()
     workportfolio.scroll.init()
+    workportfolio.postSlider.init()
+    workportfolio.toggles.init()
 }
 
 workportfolio.toggles = {
@@ -17,7 +18,8 @@ workportfolio.toggles = {
         })
 
         document.querySelectorAll('[data-toggle-target]').forEach(el => {
-            el.addEventListener('click', () => {
+            el.addEventListener('click', (e) => {
+                e.stopPropagation
                 this.toggleHandler(el)
             })
         })
@@ -44,6 +46,17 @@ workportfolio.toggles = {
 
                 item.classList.toggle('active')
                 item.querySelector('[data-toggle-item]').classList.toggle('active')
+                break
+            case ('toggleModal'):
+                const currentTarget = target.dataset.toggleTarget
+
+                if (currentTarget == 'postSlider') {
+                    workportfolio.postSlider.currentImgIndx = 0
+                    workportfolio.postSlider.start(target)
+                    document.querySelector('body').classList.toggle('bodyLock')
+                }
+
+                document.querySelector(`[data-toggle=${currentTarget}]`).classList.toggle('hidden')
                 break
         }
     }
@@ -79,6 +92,113 @@ workportfolio.scroll = {
     }
 }
 
+workportfolio.postSlider = {
+    customPost: '',
+    slidePrewArrow: '',
+    slideNextArrow: '',
+    imagesCount: 0,
+    currentImgIndx: 0,
+    images: [],
+    postImagesSrc: [],
 
-document.addEventListener('DOMContentLoaded', () => { init() }, false);
+    init: function () {
+        this.customPost = document.querySelector('[data-custom-post]')
+
+        if (this.customPost) {
+            this.postSliderHandler()
+        }
+    },
+
+    postSliderHandler: function () {
+        this.images = this.customPost.querySelector('.customPost_content').querySelectorAll('img')
+
+        this.imagesCount = this.images.length
+        this.postSliderCounter = this.customPost.querySelector('[post-slider-counter]')
+        this.postSlider = this.customPost.querySelector('.postSlider')
+        this.slidePrewArrow = this.customPost.querySelector('[data-slide-prev]')
+        this.slideNextArrow = this.customPost.querySelector('[data-slide-next]')
+
+        this.slidePrewArrow.addEventListener('click', this.getPrevImg.bind(this))
+        this.slideNextArrow.addEventListener('click', this.getNextImg.bind(this))
+
+        if (!this.imagesCount) {
+            return
+        }
+
+        this.start()
+    },
+
+    start: function (tergetImg = null) {
+        this.images.forEach((el, i) => {
+            this.postImagesSrc.push(el.src)
+            if (tergetImg === el) {
+                this.currentImgIndx = i
+            }
+
+            el.dataset.toggleTarget = 'postSlider'
+            el.dataset.type = 'toggleModal'
+
+        })
+
+        this.updateCounter()
+        this.setPostSliderImages()
+        this.triggerAnimation()
+        this.disableArrow()
+    },
+
+    getPrevImg: function () {
+        if (!this.currentImgIndx) {
+            return
+        }
+
+        this.currentImgIndx--
+        this.updateCounter()
+        this.disableArrow()
+        this.triggerAnimation()
+        this.setPostSliderImages()
+    },
+
+    getNextImg: function () {
+        if (this.imagesCount === this.currentImgIndx + 1) {
+            return
+        }
+
+        this.currentImgIndx++
+        this.updateCounter()
+        this.disableArrow()
+        this.triggerAnimation()
+        this.setPostSliderImages()
+    },
+
+    triggerAnimation: function () {
+        setTimeout(() => this.postSlider.classList.add('smooth'), 0)
+
+        this.postSlider.classList.remove('smooth')
+    },
+
+    disableArrow: function () {
+            if (!this.currentImgIndx) {
+                this.slidePrewArrow.setAttribute('disabled', '')
+                this.slideNextArrow.removeAttribute('disabled', '')
+            } else if (this.imagesCount === this.currentImgIndx + 1) {
+                this.slideNextArrow.setAttribute('disabled', '')
+                this.slidePrewArrow.removeAttribute('disabled', '') 
+            } else {
+                this.slideNextArrow.removeAttribute('disabled', '')
+                this.slidePrewArrow.removeAttribute('disabled', '') 
+            }
+    },
+
+    setPostSliderImages: function () {
+        this.postSlider.src = this.postImagesSrc[this.currentImgIndx]
+    },
+
+    updateCounter: function () {
+        const countStr = this.currentImgIndx + 1 + ' / ' + this.imagesCount
+
+        this.postSliderCounter.innerHTML = countStr
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => { init() }, false)
 
--- wp-content/themes/workportfolio/templates/custom_post.php
+++ wp-content/themes/workportfolio/templates/custom_post.php
@@ -4,47 +4,87 @@
 get_header();
 ?>
 
-<div class="customPost">
-    <div class="customPost_content">
-        <?php the_content(); ?>
+<div data-custom-post>
+    <div class='postModalSlider hidden' data-toggle='postSlider'>
+        <div class='postModalSlider_contentWrapper'>
+            <div class='slidesDashboard'>
+                <div class='slidesDashboard_count' post-slider-counter>
+                    1/2
+                </div>
+
+                <div class='slidesDashboard_close' data-toggle-target='postSlider' data-type='toggleModal'>
+                    <?php echo icons('close') ?>
+                </div>
+            </div>
+
+            <div class='postModalSlider-wrapper'>
+                <img class='postSlider' src='' />
+
+                <div class='slideArrow slide_prev' data-slide-prev>
+                    <?php echo icons('arrow') ?>
+                </div>
+
+                <div class='slideArrow slide_next' data-slide-next>
+                    <?php echo icons('arrow') ?>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <div class="customPost_text margin-left">
-        <h1 class="customPost_title">
-            <?php the_field('title'); ?>
-        </h1>
+    <div class='customPost_wrapper'>
+        <div class='customPost_card'>
+            <h1 class="customPost_title">
+                <?php the_field('title'); ?>
+            </h1>
 
-        <div class="categories_inline">
-            <?php the_category(' ') ?>
+            <div class="categories_inline">
+                <?php the_category(' ') ?>
+            </div>
+
+            <button type="button" class="secondaryButton customPostFeedbackBtn">
+                <a href="/#feedback_form">
+                    Замовити меблі
+                </a>
+            </button>
         </div>
 
-        <?php if (get_field('meta')) : ?>
-            <div class="customPost-meta">
-                <h3 class="customPost_meta">
-                    Завдання
-                </h3>
-
-                <?php the_field('meta'); ?>
+        <div class="customPost">
+            <div class="customPost_content body1">
+                <?php the_content(); ?>
             </div>
-        <?php endif ?>
 
-        <?php if (get_field('features')) : ?>
-            <h3 class="features_title">
-                Ключові особливості:
-            </h3> 
+            <div class="customPost_text margin-left">
+                <div class="customPost_text-wrapper">
+                    <?php if (get_field('meta')) : ?>
+                        <div class="customPost-meta">
+                            <h4 class="customPost_meta">
+                                Завдання
+                            </h4>
 
-            <ul class="features_list">
-                <?php get_features_list() ?>
-            </ul>
-        <?php endif ?>
+                            <p class='body1'>
+                                <?php the_field('meta'); ?>
+                            </p>
+                        </div>
+                    <?php endif ?>
 
-        <button type="button" class="secondaryButton">
-            Замовити послугу
-        </button>
+                    <?php if (get_field('features')) : ?>
+                        <h4 class="features_title">
+                            Ключові особливості:
+                        </h4>
+
+                        <ul class="features_list">
+                            <?php get_features_list() ?>
+                        </ul>
+                    <?php endif ?>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
-<?php comments_template(); ?>
+<?php
+get_footer();
+?>
 
 <?php
 function get_features_list()
@@ -52,7 +92,7 @@ function get_features_list()
     $features_arr = explode('; ', get_field('features'));
 
     foreach ($features_arr as $value) {
-        echo "<li> <span class='dash'></span>$value</li>";
+        echo "<li class='features_list-item'> <span class='dash'></span>$value</li>";
     }
 }
 ?>
